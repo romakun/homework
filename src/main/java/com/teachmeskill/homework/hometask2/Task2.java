3852c075b2ccd54144c51516bf95eedfcbbc6b27
RomaKun <roman@gophotoweb.ru>
2019-10-20 12:55:19 +0300

    Выполненное второе домашнее задание

--- src/main/java/com/teachmeskill/homework/hometask2/Task2.java
+++ src/main/java/com/teachmeskill/homework/hometask2/Task2.java
@@ -2,8 +2,7 @@ package com.teachmeskill.homework.hometask2;
 
 public class Task2 {
     public static void main(String[] args) {
-
-        for (long myNumber = 10; myNumber <= 20; myNumber++) {
+        for (int myNumber = 10; myNumber <= 20; myNumber++) {
             System.out.println("Квадрат числа " + myNumber + " = " + myNumber * myNumber);
         }
     }
